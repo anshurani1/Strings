658b0c1a2f5c4898e95472099d5e55533edbedcc
anshurani1 <62585963+anshurani1@users.noreply.github.com>
2023-08-18 13:42:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+ # my_name = "Anshu"
+# myfriend_name = "Portia"
+# another_friendname = "Aashvi"
+# print(my_name)
+# print(myfriend_name)
+# print(another_friendname)
+# my_rules = """ I am not a disciplined person, 
+# so i have committed to work half an hour perday
+# If I do not work half an hour per day, I will work ten minutes extra
+# """
+# print(my_rules)
+# print(my_name[2])
+#  my_work = "My work is to learn python and finish PhD in Neuroeconomics"
+# # print(my_work)
+# # for character in my_work:
+# #   print(character)
+#  print(len(my_work)) 
+# name = "Anshu,Pammi,Harsh,Deepika"
+# # print(name[0:5])
+# print(len(name))
+# print(nm[-4:-2])
